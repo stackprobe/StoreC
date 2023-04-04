39d32e436bbef46e64cc15a2be329f7c2823757b
stackprobe <stackprobes@gmail.com>
2023-04-04 22:41:39 +0900

    Voyager 1 is 23867901585.212 kilometers away from the Sun.

---
+++ workspace/20230404_Console/src/mains/Test0001.java
@@ -0,0 +1,16 @@
+package mains;
+
+public class Test0001 {
+	public static void main(String[] args) {
+		try {
+			test01();
+		}
+		catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void test01() {
+		System.out.println("Hello, Happy World!");
+	}
+}
